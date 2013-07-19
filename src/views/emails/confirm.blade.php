f83919dfd3a2a26a0b08ae73b7fe4cc2a479f006
Rafael Duckur <rafael.duckur@gmail.com>
2013-07-19 19:40:23 -0300

    Change password for Curso de Chacras

--- src/views/emails/confirm.blade.php
+++ src/views/emails/confirm.blade.php
@@ -2,7 +2,7 @@
 
 <p>{{ Lang::get('confide::confide.email.account_confirmation.greetings', array( 'name' => $user->firstname)) }},</p>
 <p><strong>Nome de usuário: </strong>{{ $user->username }}</p>
-<p><strong>Senha: </strong>cursopa13</p>
+<p><strong>Senha: </strong>cursocc13</p>
 
 <p>{{ Lang::get('confide::confide.email.account_confirmation.body') }}</p>
 <a href='{{{ URL::to("user/confirm/{$user->confirmation_code}") }}}'>
